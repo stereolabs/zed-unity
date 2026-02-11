7fa0cd140352a93b539210ea113754a84470fa39
bvallon <benjamin.vallon@stereolabs.com>
2026-02-11 11:21:36 +0100

    add missing error code

--- ZEDCamera/Assets/SDK/NativeInterface/ZEDCommon.cs
+++ ZEDCamera/Assets/SDK/NativeInterface/ZEDCommon.cs
@@ -938,6 +938,12 @@ namespace sl
     public enum ERROR_CODE
     {
         /// <summary>
+        /// The sensor's configuration (mode, multicast, etc.) was changed externally while streaming.
+        /// If auto_recovery_on_config_change is enabled, the SDK will automatically reconnect.
+        /// This warning code is returned once after successful recovery.
+        /// </summary>
+        SENSOR_CONFIGURATION_CHANGED = -6,
+        /// <summary>
         /// The camera has a potential calibration issue.
         /// </summary>
         POTENTIAL_CALIBRATION_ISSUE = -5,
